31e2f5b2c29ba0e86e93a682ee6e567291fdd6cb
Gitanjali Yadav <gitanjaliyadav1437@gmail.com>
2025-07-21 03:10:29 +0530

    added Home page deign and interview cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import {dummyInterviews} from "@/constants";
+import InterviewCard from "@/components/InterviewCard";
 
 const Page = () => {
     return (
-        <div>Home Page</div>
+        <>
+        <section className="card-cta">
+            <div className="flex flex-col gap-6 max-w-lg">
+                <h2>Get Interview Ready with AI-powered Practice & Feedback</h2>
+                <p className="text-lg">Practice on real Interview Questions & get instant Feedback.</p>
+                <Button asChild className="btn-primary max-sm:w-full">
+                    <Link href="/interview">Start an Interview</Link>
+                </Button>
+            </div>
+
+            <Image src="/img_1.png" alt="robo-dude" width={400} height={400} className="max-sm:hidden rounded-2xl shadow-xl hover:scale-105 transition-transform duration-300 ease-in-out border-[6px] border-white outline outline-[3px] outline-white/70 shadow-[0_0_20px_5px_rgba(255,255,255,0.5)]" />
+        </section>
+
+            <section className="flex flex-col gap-6 mt-8 ">
+                <h2>Your Interviews</h2>
+                <div className="interviews-section">
+
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id}
+                        />
+                    ))}
+                </div>
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8 ">
+                <h2>Take an Interview </h2>
+                <div className="interviews-section">
+                    {dummyInterviews.map((interview) => (
+                        <InterviewCard {...interview} key={interview.id}
+                                        />
+                    ))}
+                    {/*<p>You haven&apos;t taken any interviews yet!</p>*/}
+                </div>
+            </section>
+        </>
     )
 }
 export default Page
